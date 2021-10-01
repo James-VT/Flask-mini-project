455513be403d711bc483a4d4273d9fc653615446
James-VT <jd_tanner@hotmail.co.uk>
2021-10-01 09:30:30 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $(".datepicker").datepicker({
+      format: "dd mmmm, yyyy"
+    });
   });
